e027c86c1b24aab49813f7ec21cf3a31cd0f14d3
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-05-28 18:55:13 +0530

    Update SegregatingEven_OddNumbersInArray.java

--- SegregatingEven_OddNumbersInArray.java
+++ SegregatingEven_OddNumbersInArray.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 class SegregatingEven_OddNumbersInArray{
 	public static void main(String args[]){
+
 		Scanner sc=new Scanner(System.in);
+
 		System.out.print("\nEnter the size of array > ");
 		int size=sc.nextInt();
-		if(size<0){
-			System.out.println("\nInvalid Size..!");
+
+		if(size<=0){
+			System.out.println("\n"+size+" -> Array size must be greater than zero!!");
+			sc.close();
 			return;
 		}
+		
 		int a[]=new int[size];
 		System.out.println("\nEnter the elements >");
 		for(int i=0;i<a.length;++i){
