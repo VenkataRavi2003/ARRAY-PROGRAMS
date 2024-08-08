8fc4d2a722ddfcb79184d9a17e64b09fd1c91225
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-08-08 07:13:33 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,58 +1,56 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class TwoSum{
 	public static void main(String[] args) {
 		
-		Scanner sc  = new Scanner(System.in);
+		try(Scanner sc = new Scanner(System.in)){
 
-		System.out.print("\nEnter the size of array : ");
-		int size = sc.nextInt();
+			System.out.print("\nEnter the size of array : ");
+			int size = sc.nextInt();
 
-		if(size <= 0){
-			System.out.println("\n"+size+" -> size must be greater than zero!!");
-			return;
+			if(size <= 0){
+				System.out.println("\n"+size+" -> array size must be greater than zero!!");
+				return;
+			}
+
+			int arr[] = new int[size];
+
+			System.out.println("\nEnter the elements into array : ");
+			takeInput(arr,sc);
+
+			System.out.println("\nResulted Array : \n"+Arrays.toString(arr));
+
+			System.out.print("\nEnter the target sum -> ");
+			int targetSum = sc.nextInt();
+
+			int result[] = findTwoSum(arr,targetSum);
+			
+			if (result[0] == -1 && result[1] == -1) {
+                System.out.println("\nNo two sum solution found.");
+            } else {
+                System.out.println("\nTwo Indices Are: " + result[0] + " & " + result[1]);
+            }
+
+		}catch (Exception e) {
+			System.out.println("\nException Occured :: "+e.getMessage());
 		}
-
-		int arr[] = new int[size];
-
-		System.out.println("\nEnter the elements into array : ");
-		takeInput(arr,sc,size);
-
-		System.out.println("\nResulted Array : ");
-		printArray(arr);
-
-		System.out.print("\nEnter the target sum : ");
-		int targetSum = sc.nextInt();
-
-		int result[] = twoSum(arr,targetSum);
-
-		System.out.println("\nIndicies : "+result[0]+" && "+result[1]);
-
-		sc.close();
 	}
-	public static void takeInput(int arr[] , Scanner sc,int size){
-
-		for (int i = 0 ; i < size ; ++i) {
+	public static void takeInput(int arr[],Scanner sc){
+		for (int i = 0 ; i < arr.length ; ++i) {
 			arr[i] = sc.nextInt();
 		}
 	}
-	public static void printArray(int arr[]){
-		for (int item : arr) {
-			System.out.print(item+" ");
-		}
-	}
-	public static int[] twoSum(int arr[],int targetSum){
+	public static int[] findTwoSum(int arr[],int targetSum){
 
 		for (int i = 0 ; i < arr.length ; ++i) {
-			
-			for (int j = i + 1 ; j < arr.length  ; ++j) {
+			for (int j = i + 1 ; j < arr.length ; ++j) {
 				
 				if(arr[i] + arr[j] == targetSum){
 
-					return new int[] {i , j};
+					return new int[]{i,j};
 				}
 			}
 		}
-
-		throw new IllegalArgumentException("No two sum solution!!");
+		return new int[]{-1,-1};
 	}
 }
